32a0cc92a4ab6922dd9558ae7583cd001601d1a5
Xin Liu <sam@secondstate.io>
2022-07-21 22:47:33 +0800

    [openvino][refactor](openvino-road-seg-adas): Introduced image_to_tensor func. (#7)

--- openvino-road-segmentation-adas/rust/openvino-road-seg-adas/src/main.rs
+++ openvino-road-segmentation-adas/rust/openvino-road-seg-adas/src/main.rs
@@ -1,17 +1,19 @@
-use std::{env, fs};
+use image::{io::Reader, DynamicImage};
+use std::env;
 use wasmedge_nn::nn::{ctx::WasiNnCtx, Dtype, ExecutionTarget, GraphEncoding, Tensor};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let model_xml_name: &str = &args[1];
     let model_bin_name: &str = &args[2];
-    let tensor_name: &str = &args[3];
+    let image_name: &str = &args[3];
 
-    let tensor_data = fs::read(tensor_name).unwrap();
+    // load image
+    let tensor_data = image_to_tensor(image_name.to_string(), 512, 896);
     println!("Load input tensor, size in bytes: {}", tensor_data.len());
     let tensor = Tensor {
         dimensions: &[1, 3, 512, 896],
-        r#type: Dtype::F32.into(), // wasi_nn::TENSOR_TYPE_F32,
+        r#type: Dtype::F32.into(),
         data: &tensor_data,
     };
 
@@ -65,4 +67,28 @@ fn dump<T>(
     std::fs::write(path, &dst_slice).expect("failed to write file");
 
     Ok(())
+}
+
+// Take the image located at 'path', open it, resize it to height x width, and then converts
+// the pixel precision to FP32. The resulting BGR pixel vector is then returned.
+fn image_to_tensor(path: String, height: u32, width: u32) -> Vec<u8> {
+    let pixels = Reader::open(path).unwrap().decode().unwrap();
+    let dyn_img: DynamicImage = pixels.resize_exact(width, height, image::imageops::Triangle);
+    let bgr_img = dyn_img.to_bgr8();
+    // Get an array of the pixel values
+    let raw_u8_arr: &[u8] = &bgr_img.as_raw()[..];
+    // Create an array to hold the f32 value of those pixels
+    let bytes_required = raw_u8_arr.len() * 4;
+    let mut u8_f32_arr: Vec<u8> = vec![0; bytes_required];
+
+    for i in 0..raw_u8_arr.len() {
+        // Read the number as a f32 and break it into u8 bytes
+        let u8_f32: f32 = raw_u8_arr[i] as f32;
+        let u8_bytes = u8_f32.to_ne_bytes();
+
+        for j in 0..4 {
+            u8_f32_arr[(i * 4) + j] = u8_bytes[j];
+        }
+    }
+    return u8_f32_arr;
 }
