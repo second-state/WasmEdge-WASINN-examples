0ff9974fd2fa8a3cea600337dc6a94d9aecab6a9
Jianbai Ye <jianbaiye@outlook.com>
2022-10-17 14:15:43 +0800

    fix: remove model
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- tflite-birds_v1-image/download_data.sh
+++ tflite-birds_v1-image/download_data.sh
@@ -4,6 +4,3 @@ TODIR=$1
 if [ ! -f $TODIR/bird.jpg ]; then
     wget --no-clobber --directory-prefix=$TODIR $FIXTURE/bird.jpg
 fi
-if [ ! -f $TODIR/lite-model_aiy_vision_classifier_birds_V1_3.tflite ]; then
-    wget --no-clobber --directory-prefix=$TODIR $FIXTURE/lite-model_aiy_vision_classifier_birds_V1_3.tflite
-fi
