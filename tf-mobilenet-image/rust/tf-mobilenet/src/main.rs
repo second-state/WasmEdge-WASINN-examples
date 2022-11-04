5a2161a2c5091ad9f7ea83420fa5f63edc6bc287
Jianbai Ye <jianbaiye@outlook.com>
2022-11-04 11:30:00 +0800

    feat: change name
    Signed-off-by: Jianbai Ye <jianbaiye@outlook.com>

--- tf-mobilenet-image/rust/tf-mobilenet/src/main.rs
+++ tf-mobilenet-image/rust/tf-mobilenet/src/main.rs
@@ -1,8 +1,8 @@
-use image::io::Reader;
-use image::DynamicImage;
 use std::convert::TryInto;
 use std::env;
 use std::fs;
+use std::fs::File;
+use std::io::Read;
 use wasi_nn;
 mod imagenet_classes;
 
@@ -53,13 +53,13 @@ fn infer_image() {
     }
     println!("Executed graph inference");
     // Retrieve the output.
-    let mut output_buffer = vec![0u8; 965];
+    let mut output_buffer = vec![0f32; 965];
     unsafe {
         wasi_nn::get_output(
             context,
             0,
-            &mut output_buffer[..] as *mut [u8] as *mut u8,
-            output_buffer.len().try_into().unwrap(),
+            &mut output_buffer[..] as *mut [f32] as *mut u8,
+            (output_buffer.len() * 4).try_into().unwrap(),
         )
         .unwrap();
     }
@@ -79,7 +79,7 @@ fn infer_image() {
 // Sort the buffer of probabilities. The graph places the match probability for each class at the
 // index for that class (e.g. the probability of class 42 is placed at buffer[42]). Here we convert
 // to a wrapping InferenceResult and sort the results.
-fn sort_results(buffer: &[u8]) -> Vec<InferenceResult> {
+fn sort_results(buffer: &[f32]) -> Vec<InferenceResult> {
     let mut results: Vec<InferenceResult> = buffer
         .iter()
         .enumerate()
@@ -92,15 +92,35 @@ fn sort_results(buffer: &[u8]) -> Vec<InferenceResult> {
 // Take the image located at 'path', open it, resize it to height x width, and then converts
 // the pixel precision to FP32. The resulting BGR pixel vector is then returned.
 fn image_to_tensor(path: String, height: u32, width: u32) -> Vec<u8> {
-    let pixels = Reader::open(path).unwrap().decode().unwrap();
-    let dyn_img: DynamicImage = pixels.resize_exact(width, height, image::imageops::Triangle);
-    let bgr_img = dyn_img.to_rgb8();
-    // Get an array of the pixel values
-    let raw_u8_arr: &[u8] = &bgr_img.as_raw()[..];
-    let u8_f32_arr = raw_u8_arr.to_vec();
+    let mut file_img = File::open(path).unwrap();
+    let mut img_buf = Vec::new();
+    file_img.read_to_end(&mut img_buf).unwrap();
+    let img = image::load_from_memory(&img_buf).unwrap().to_rgb8();
+    let resized =
+        image::imageops::resize(&img, height, width, ::image::imageops::FilterType::Triangle);
+    let mut flat_img: Vec<f32> = Vec::new();
+    for rgb in resized.pixels() {
+        flat_img.push(rgb[0] as f32 / 255.);
+        flat_img.push(rgb[1] as f32 / 255.);
+        flat_img.push(rgb[2] as f32 / 255.);
+    }
+    let bytes_required = flat_img.len() * 4;
+    let mut u8_f32_arr: Vec<u8> = vec![0; bytes_required];
+
+    for c in 0..3 {
+        for i in 0..(flat_img.len() / 3) {
+            // Read the number as a f32 and break it into u8 bytes
+            let u8_f32: f32 = flat_img[i * 3 + c] as f32;
+            let u8_bytes = u8_f32.to_ne_bytes();
+
+            for j in 0..4 {
+                u8_f32_arr[((flat_img.len() / 3 * c + i) * 4) + j] = u8_bytes[j];
+            }
+        }
+    }
     return u8_f32_arr;
 }
 
 // A wrapper for class ID and match probabilities.
 #[derive(Debug, PartialEq)]
-struct InferenceResult(usize, u8);
+struct InferenceResult(usize, f32);
