8e769d0a93f525fec22d4971c5a425360f0bc1a8
ye jianbai <your_email@abc.example>
2023-03-03 16:24:15 +0800

    chore: add image
    Signed-off-by: ye jianbai <your_email@abc.example>

--- tf-mobilenet-image/download_data.sh
+++
@@ -1,6 +0,0 @@
-FIXTURE=https://raw.githubusercontent.com/second-state/wasm-learning/master/rust/birds_v1/
-TODIR=$1
-
-if [ ! -f $TODIR/bird.jpg ]; then
-    wget --no-clobber --directory-prefix=$TODIR $FIXTURE/bird.jpg
-fi
