5243f9a1dbd7cbc1ce5abc173274bfdd72c90861
Divyanshu GUpta <divyanshugupta585@gmail.com>
2023-07-24 16:18:57 +0530

    Removed unnecessary code
    Signed-off-by: Divyanshu GUpta <divyanshugupta585@gmail.com>

--- scripts/install_openvino.sh
+++ scripts/install_openvino.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 echo "Installing OpenVINO with version 2023.0.0"
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
@@ -8,14 +7,3 @@ echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu20 main" | tee /etc/ap
 apt update
 apt-get -y install openvino
 ldconfig
-
-echo "*** inspect /etc/ld.so.conf"
-cat /etc/ld.so.conf
-
-echo "*** add new paths to /etc/ld.so.conf"
-echo "include /etc/ld.so.conf.d/*.conf" >> /etc/ld.so.conf
-echo "/opt/intel/_2021/deployment_tools/ngraph/lib/" >> /etc/ld.so.conf
-echo "/opt/intel/openvino_2021/deployment_tools/inference_engine/lib/intel64/" >> /etc/ld.so.conf
-
-echo "*** inspect /etc/ld.so.conf"
-cat /etc/ld.so.conf
