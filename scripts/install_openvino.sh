8f41152870a079a8c25ced02af463e310834f4ed
hydai <z54981220@gmail.com>
2023-09-20 11:16:41 +0800

    [CI] Use openvino-2023.0.0
    Signed-off-by: hydai <z54981220@gmail.com>

--- scripts/install_openvino.sh
+++ scripts/install_openvino.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -e
-echo "Installing OpenVINO with version 2023"
+echo "Installing OpenVINO with version 2023.0.0"
 wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-2023.list
 apt update
 apt upgrade -y
 apt search openvino
-apt-get -y install python3
-apt-get -y install openvino
+apt-get -y install openvino-2023.0.0
 export PATH=/usr/lib/x86_64-linux-gnu:$PATH
