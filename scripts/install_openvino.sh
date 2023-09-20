ed1957b05e35f165573732280a5d1cb7b63aa563
hydai <z54981220@gmail.com>
2023-09-20 10:52:22 +0800

    [Script] OpenVINO 2023 relies on libpython3.8
    Signed-off-by: hydai <z54981220@gmail.com>

--- scripts/install_openvino.sh
+++ scripts/install_openvino.sh
@@ -5,6 +5,8 @@ wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PU
 apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
 echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-2023.list
 apt update
+apt upgrade -y
+apt-get -y install python3
 apt-get -y install openvino
 ldconfig
 export PATH=/usr/lib/x86_64-linux-gnu:$PATH
