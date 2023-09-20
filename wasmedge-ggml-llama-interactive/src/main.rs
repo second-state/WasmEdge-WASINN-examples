b779c59ff23f1062487c1baf46a35fd812badbe3
dm4 <dm4@secondstate.io>
2023-09-20 11:58:56 +0000

    [Examples] Add the interactive llama example
    Signed-off-by: dm4 <dm4@secondstate.io>

---
+++ wasmedge-ggml-llama-interactive/src/main.rs
@@ -0,0 +1,55 @@
+use std::env;
+use std::io;
+use wasi_nn;
+
+fn read_input() -> String {
+    loop {
+        let mut answer = String::new();
+        io::stdin()
+            .read_line(&mut answer)
+            .ok()
+            .expect("Failed to read line");
+        if !answer.is_empty() && answer != "\n" && answer != "\r\n" {
+            return answer;
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let model_name: &str = &args[1];
+
+    let graph =
+        wasi_nn::GraphBuilder::new(wasi_nn::GraphEncoding::Ggml, wasi_nn::ExecutionTarget::CPU)
+            .build_from_cache(model_name)
+            .unwrap();
+    let mut context = graph.init_execution_context().unwrap();
+
+    let system_prompt = String::from("<<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe. <</SYS>>\n\n");
+    let mut saved_prompt = String::new();
+
+    loop {
+        println!("Question:");
+        let input = read_input();
+        if saved_prompt == "" {
+            saved_prompt = format!("[INST] {} {} [/INST]", system_prompt, input.trim());
+        } else {
+            saved_prompt = format!("{} [INST] {} [/INST]", saved_prompt, input.trim());
+        }
+
+        // Set prompt to the input tensor.
+        let tensor_data = saved_prompt.as_bytes().to_vec();
+        context
+            .set_input(0, wasi_nn::TensorType::U8, &[1], &tensor_data)
+            .unwrap();
+
+        // Execute the inference.
+        context.compute().unwrap();
+
+        // Retrieve the output.
+        let mut output_buffer = vec![0u8; 1000];
+        context.get_output(0, &mut output_buffer).unwrap();
+        let output = String::from_utf8(output_buffer.clone()).unwrap();
+        println!("Answer:\n{}", output.trim());
+    }
+}
