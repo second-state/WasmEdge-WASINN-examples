c1d4dbad4180b2039f1861643ab5e4f1250b7262
dm4 <dm4@secondstate.io>
2024-01-09 11:50:25 +0800

    [Example] ggml: refactor
    Signed-off-by: dm4 <dm4@secondstate.io>

--- wasmedge-ggml-llama-interactive/src/main.rs
+++ wasmedge-ggml-llama-interactive/src/main.rs
@@ -1,7 +1,7 @@
 use serde_json::{json, Value};
 use std::env;
 use std::io::{self, Write};
-use wasi_nn;
+use wasi_nn::{self, GraphExecutionContext};
 
 fn read_input() -> String {
     loop {
@@ -15,14 +15,7 @@ fn read_input() -> String {
     }
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let model_name: &str = &args[1];
-
-    // Preserve for 4096 tokens with average token length 6
-    const MAX_OUTPUT_BUFFER_SIZE: usize = 4096 * 6;
-
-    let mut stream_stdout = false;
+fn get_options_from_env() -> Value {
     let mut options = json!({});
     match env::var("enable_log") {
         Ok(val) => options["enable-log"] = serde_json::from_str(val.as_str()).unwrap(),
@@ -33,10 +26,7 @@ fn main() {
         _ => (),
     };
     match env::var("stream_stdout") {
-        Ok(val) => {
-            options["stream-stdout"] = serde_json::from_str(val.as_str()).unwrap();
-            stream_stdout = options["stream-stdout"].as_bool().unwrap();
-        },
+        Ok(val) => options["stream-stdout"] = serde_json::from_str(val.as_str()).unwrap(),
         _ => (),
     };
     match env::var("n_predict") {
@@ -72,6 +62,57 @@ fn main() {
         _ => (),
     }
 
+    return options;
+}
+
+fn set_data_to_context(
+    context: &mut GraphExecutionContext,
+    data: Vec<u8>,
+) -> Result<(), wasi_nn::Error> {
+    context.set_input(0, wasi_nn::TensorType::U8, &[1], &data)
+}
+
+#[allow(dead_code)]
+fn set_metadata_to_context(
+    context: &mut GraphExecutionContext,
+    data: Vec<u8>,
+) -> Result<(), wasi_nn::Error> {
+    context.set_input(1, wasi_nn::TensorType::U8, &[1], &data)
+}
+
+fn get_data_from_context(
+    context: &GraphExecutionContext,
+    index: usize,
+    is_compute_single: bool,
+) -> String {
+    // Preserve for 4096 tokens with average token length 6
+    const MAX_OUTPUT_BUFFER_SIZE: usize = 4096 * 6;
+    let mut output_buffer = vec![0u8; MAX_OUTPUT_BUFFER_SIZE];
+    let mut output_size = if is_compute_single {
+        context
+            .get_output_single(index, &mut output_buffer)
+            .unwrap()
+    } else {
+        context.get_output(index, &mut output_buffer).unwrap()
+    };
+    output_size = std::cmp::min(MAX_OUTPUT_BUFFER_SIZE, output_size);
+
+    return String::from_utf8_lossy(&output_buffer[..output_size]).to_string();
+}
+
+fn get_output_from_context(context: &GraphExecutionContext, is_compute_single: bool) -> String {
+    return get_data_from_context(context, 0, is_compute_single);
+}
+
+fn get_metadata_from_context(context: &GraphExecutionContext) -> Value {
+    return serde_json::from_str(&get_data_from_context(context, 1, false)).unwrap();
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let model_name: &str = &args[1];
+    let options = get_options_from_env();
+
     // We suuport both compute() and compute_single().
     // compute() will compute the entire paragraph until the end of sequence, and return the entire output.
     // compute_single() will compute one token at a time, and return the output of the last token.
@@ -88,20 +129,10 @@ fn main() {
             .unwrap();
     let mut context = graph.init_execution_context().unwrap();
 
-    let system_prompt = String::from("<<SYS>>You are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe. <</SYS>>");
-    let mut saved_prompt = String::new();
-
     // We also support setting the options via input tensor with index 1.
-    // Check our README for more details.
+    // Uncomment the line below to run the example, Check our README for more details.
     //
-    // context
-    //     .set_input(
-    //         1,
-    //         wasi_nn::TensorType::U8,
-    //         &[1],
-    //         &options.to_string().as_bytes().to_vec(),
-    //     )
-    //     .unwrap();
+    // set_metadata_to_context(&mut context, options.to_string().as_bytes().to_vec()).unwrap();
 
     // If there is a third argument, use it as the prompt and enter non-interactive mode.
     // Otherwise, enter interactive mode.
@@ -114,108 +145,89 @@ fn main() {
             .unwrap();
         println!("Response:");
         context.compute().unwrap();
-        let mut output_buffer = vec![0u8; MAX_OUTPUT_BUFFER_SIZE];
-        let mut output_size = context.get_output(0, &mut output_buffer).unwrap();
-        output_size = std::cmp::min(MAX_OUTPUT_BUFFER_SIZE, output_size);
-        let output = String::from_utf8_lossy(&output_buffer[..output_size]).to_string();
+        let output = get_output_from_context(&context, false);
         println!("{}", output.trim());
-    } else {
-        loop {
-            println!("Question:");
-            let input = read_input();
-            if saved_prompt == "" {
-                saved_prompt = format!("[INST] {} {} [/INST]", system_prompt, input.trim());
-            } else {
-                saved_prompt = format!("{} [INST] {} [/INST]", saved_prompt, input.trim());
-            }
+        std::process::exit(0);
+    }
 
-            // Set prompt to the input tensor.
-            let tensor_data = saved_prompt.as_bytes().to_vec();
-            context
-                .set_input(0, wasi_nn::TensorType::U8, &[1], &tensor_data)
-                .unwrap();
+    let system_prompt = String::from("<<SYS>>You are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe. <</SYS>>");
+    let mut saved_prompt = String::new();
 
-            // Get the number of input tokens.
-            let mut input_metadata_buffer = vec![0u8; MAX_OUTPUT_BUFFER_SIZE];
-            let mut input_metadata_size =
-                context.get_output(1, &mut input_metadata_buffer).unwrap();
-            input_metadata_size = std::cmp::min(MAX_OUTPUT_BUFFER_SIZE, input_metadata_size);
-            let input_metadata_str =
-                String::from_utf8_lossy(&input_metadata_buffer[..input_metadata_size]).to_string();
-            let input_metadata: Value = serde_json::from_str(&input_metadata_str).unwrap();
-            if let Some(true) = options["enable-log"].as_bool() {
-                println!("Number of input tokens: {}", input_metadata["input_tokens"]);
-            }
+    loop {
+        println!("Question:");
+        let input = read_input();
+        if saved_prompt == "" {
+            saved_prompt = format!("[INST] {} {} [/INST]", system_prompt, input.trim());
+        } else {
+            saved_prompt = format!("{} [INST] {} [/INST]", saved_prompt, input.trim());
+        }
 
-            println!("Answer:");
+        // Set prompt to the input tensor.
+        set_data_to_context(&mut context, saved_prompt.as_bytes().to_vec()).unwrap();
 
-            let mut output = String::new();
-            if is_compute_single {
-                // Compute one token at a time, and get the token using the get_output_single().
-                loop {
-                    let result = context.compute_single();
-                    match result {
-                        Ok(_) => (),
-                        Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::EndOfSequence)) => {
-                            break;
-                        }
-                        Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::ContextFull)) => {
-                            println!("[INFO] Context full");
-                            break;
-                        }
-                        Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::PromptTooLong)) => {
-                            println!("[INFO] Prompt too long");
-                            break;
-                        }
-                        Err(err) => {
-                            println!("[ERROR] {}", err);
-                            break;
-                        }
+        // Get the number of input tokens.
+        let input_metadata = get_metadata_from_context(&context);
+        if let Some(true) = options["enable-log"].as_bool() {
+            println!("Number of input tokens: {}", input_metadata["input_tokens"]);
+        }
+
+        println!("Answer:");
+
+        let mut output = String::new();
+        if is_compute_single {
+            // Compute one token at a time, and get the token using the get_output_single().
+            loop {
+                match context.compute_single() {
+                    Ok(_) => (),
+                    Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::EndOfSequence)) => {
+                        break;
                     }
-                    // Retrieve the output.
-                    let mut output_buffer = vec![0u8; MAX_OUTPUT_BUFFER_SIZE];
-                    let mut output_size = context.get_output_single(0, &mut output_buffer).unwrap();
-                    output_size = std::cmp::min(MAX_OUTPUT_BUFFER_SIZE, output_size);
-                    let token = String::from_utf8_lossy(&output_buffer[..output_size]).to_string();
-                    print!("{}", token);
-                    io::stdout().flush().unwrap();
-                    output += &token;
+                    Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::ContextFull)) => {
+                        println!("[INFO] Context full");
+                        break;
+                    }
+                    Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::PromptTooLong)) => {
+                        println!("[INFO] Prompt too long");
+                        break;
+                    }
+                    Err(err) => {
+                        println!("[ERROR] {}", err);
+                        break;
+                    }
                 }
-                println!("");
-            } else {
-                // Execute the inference.
-                context.compute().unwrap();
-
                 // Retrieve the output.
-                let mut output_buffer = vec![0u8; MAX_OUTPUT_BUFFER_SIZE];
-                let mut output_size = context.get_output(0, &mut output_buffer).unwrap();
-                output_size = std::cmp::min(MAX_OUTPUT_BUFFER_SIZE, output_size);
-                output = String::from_utf8_lossy(&output_buffer[..output_size]).to_string();
-                if !stream_stdout {
-                    println!("{}", output.trim());
-                } else {
-                    println!("");
-                }
+                let token = get_output_from_context(&context, is_compute_single);
+                print!("{}", token);
+                io::stdout().flush().unwrap();
+                output += &token;
             }
+            println!("");
+        } else {
+            // Blocking: execute the inference.
+            context.compute().unwrap();
 
-            saved_prompt = format!("{} {} ", saved_prompt, output.trim());
+            // Retrieve the output.
+            output = get_output_from_context(&context, is_compute_single);
 
-            // Retrieve the output metadata.
-            let mut metadata_buffer = vec![0u8; MAX_OUTPUT_BUFFER_SIZE];
-            let mut metadata_size = context.get_output(1, &mut metadata_buffer).unwrap();
-            metadata_size = std::cmp::min(MAX_OUTPUT_BUFFER_SIZE, metadata_size);
-            let metadata_str =
-                String::from_utf8_lossy(&metadata_buffer[..metadata_size]).to_string();
-            let metadata: Value = serde_json::from_str(&metadata_str).unwrap();
-            if let Some(true) = options["enable-log"].as_bool() {
-                println!("Number of input tokens: {}", metadata["input_tokens"]);
-                println!("Number of output tokens: {}", metadata["output_tokens"]);
+            // Print the output if not streaming.
+            if !options["stream-stdout"].as_bool().unwrap() {
+                print!("{}", output.trim());
             }
+            println!("");
+        }
 
-            // Delete context.
-            if is_compute_single {
-                context.fini_single().unwrap();
-            }
+        saved_prompt = format!("{} {} ", saved_prompt, output.trim());
+
+        // Retrieve the output metadata.
+        let metadata = get_metadata_from_context(&context);
+        if let Some(true) = options["enable-log"].as_bool() {
+            println!("Number of input tokens: {}", metadata["input_tokens"]);
+            println!("Number of output tokens: {}", metadata["output_tokens"]);
+        }
+
+        // Delete context.
+        if is_compute_single {
+            context.fini_single().unwrap();
         }
     }
 }
