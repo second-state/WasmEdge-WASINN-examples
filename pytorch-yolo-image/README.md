23d79270770f334974cb8593e853e1a211e146e1
C Schleich <schleich.cdaniel@gmail.com>
2024-02-02 20:07:04 +0100

    [Example] Create pytorch-yolo-image example (#35)
    * [Example] Create pytorch-yolo-image dir, get pretrained YOLO weights via python script, begin add rust project to load in weights, pre-process image and post process Tensor, WIP
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    Image formatting, interpret output of neural net forward pass WIP
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    [Example] Swap input Red and Blue Channels, chunk and transpose output from neural network to correct dimensions
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    [Example] Add YOLO Classes, Processing YOLO output, Add transpose function, match maximal probability of prediction to class
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    [Example] Yolo example, Cleanup
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    [Example] removed opencvmini dependency, cleaup, update README.md
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    [Example] Update Readme
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    [Example] Cleanup of code, use default cheese banana image
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>
    
    * [Example] Update Readme, Add githubworkflow file
    
    * [Example] fixed incorrect result scaling to original image dimensions
    
    * [Example] update workflow install process for Libtorch, update build and run for python-yolo-detection
    
    * [Example] workflow: add wasi_nn-pytorch plugin to wasmedge, fix workflows pipeline: copy libtorch to /lib/
    
    ---------
    
    Signed-off-by: Charles Schleich <schleich.cdaniel@gmail.com>

---
+++ pytorch-yolo-image/README.md
@@ -0,0 +1,30 @@
+# YOLOV8 detection Example For WASI-NN with PyTorch Backend
+
+Code for a working example of object detection using the yolov8 model. 
+
+### Requirements 
+
+`pip3 install torchvision ultralytics`  
+
+To get the Pytorch YOLOV8 model please use the `get_model.py` script provided. 
+
+Alternatively: pre-trained weights can be downloaded directly from github and will need to be converted to torchscript
+https://github.com/ultralytics/assets/releases
+
+### Build + Run
+
+To build (from the `/rust` directory):  
+`cargo build --release --target wasm32-wasi`
+
+To compile the WASM AOT (Ahead of Time) - this results in a much more performant binary  
+`
+wasmedge compile ./target/wasm32-wasi/release/wasmedge-wasinn-example-yolo-image.wasm ./target/wasm32-wasi/release/wasmedge-wasinn-example-yolo-image.wasm
+`
+
+To Run (from root `/` directory of the example):  
+`
+wasmedge --dir .:. ./rust/target/wasm32-wasi/release/wasmedge-wasinn-example-yolo-image.wasm ./yolov8n.torchscript ./input.jpg
+`
+
+> **Note**
+> Non-Maximum supression has not been applied to the output, therefore there will be many results with very close pixel bounds
--- scripts/install_libtorch.sh
+++ scripts/install_libtorch.sh
@@ -6,10 +6,16 @@ if [[ ! -n ${PYTORCH_VERSION} ]]; then
   PYTORCH_VERSION="1.8.2"
 fi
 
+
 DOWNLOAD_TO=$1
 if [ ! -d $1/libtorch ]; then
-    curl -s -L -O --remote-name-all https://download.pytorch.org/libtorch/lts/1.8/cpu/libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip
-    echo "b76d6dd4380e2233ce6f7654e672e13aae7c871231d223a4267ef018dcbfb616 libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip" | sha256sum -c
-    unzip -q "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
-    rm -f "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+  apt update
+  apt upgrade -y
+  apt-get install unzip wget -y
+  curl -L -O https://download.pytorch.org/libtorch/lts/1.8/cpu/libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip
+  echo "Run Checksum..."
+  echo "b76d6dd4380e2233ce6f7654e672e13aae7c871231d223a4267ef018dcbfb616 libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip" | sha256sum -c
+  unzip -q "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+  rm -f "libtorch-cxx11-abi-shared-with-deps-${PYTORCH_VERSION}%2Bcpu.zip"
+  echo "Complete install"
 fi
