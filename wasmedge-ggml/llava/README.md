15f5ace725d40a3a026ab94def92587ee9dd194b
dm4 <dm4@secondstate.io>
2024-02-16 12:40:16 +0800

    [Example] ggml: add llava support
    Signed-off-by: dm4 <dm4@secondstate.io>

---
+++ wasmedge-ggml/llava/README.md
@@ -0,0 +1,61 @@
+# Llava Example For WASI-NN with GGML Backend
+
+> [!NOTE]
+> Please refer to the [wasmedge-ggml/README.md](../README.md) for the general introduction and the setup of the WASI-NN plugin with GGML backend. This document will focus on the specific example of the Llava model.
+
+## Get Llava Model
+
+In this example, we are going to use the pre-converted [llava-v1.5-7b](https://huggingface.co/mys/ggml_llava-v1.5-7b) model.
+
+Download the llava model:
+
+```bash
+curl -LO https://huggingface.co/mys/ggml_llava-v1.5-7b/resolve/main/ggml-model-q5_k.gguf
+curl -LO https://huggingface.co/mys/ggml_llava-v1.5-7b/resolve/main/mmproj-model-f16.gguf
+```
+
+## Prepare the Image
+
+Download the image for the Llava model:
+
+```bash
+curl -LO https://llava-vl.github.io/static/images/monalisa.jpg
+```
+
+## Parameters
+
+> [!NOTE]
+> Please check the parameters section of [wasmedge-ggml/README.md](https://github.com/second-state/WasmEdge-WASINN-examples/tree/master/wasmedge-ggml#parameters) first.
+
+For GPU offloading, please adjust the `n-gpu-layers` options to the number of layers that you want to offload to the GPU.
+
+```rust
+options.insert("n-gpu-layers", Value::from(...));
+```
+
+In llava inference, we recommend to use the `ctx-size` at least `2048` for better performance.
+
+```rust
+options.insert("ctx-size", Value::from(2048));
+```
+
+## Execute
+
+Execute the WASM with the `wasmedge` using the named model feature to preload a large model:
+
+```console
+$ wasmedge --dir .:. \
+  --env mmproj=mmproj-model-f16.gguf \
+  --env image=monalisa.jpg \
+  --nn-preload default:GGML:AUTO:ggml-model-q5_k.gguf \
+  wasmedge-ggml-llava.wasm default
+
+Question:
+what is in this picture?
+Answer:
+The image features a painting or portrait of a woman with long hair, likely inspired by the famous Mona Lisa artwork. She appears to be smiling and is surrounded by an ocean view, giving the impression of being on a boat. The scene is painted in black and white, creating a classic and timeless look. Additionally, there are two birds present within this painting, one on the left side towards the top and another slightly higher up in the center.
+Question:
+Do you know who drew this painting?
+Answer:
+As a visual AI, I don't have knowledge about the artist who created this painting. However, it is inspired by the famous Mona Lisa artwork, which was painted by Leonardo da Vinci.
+```
---
+++ wasmedge-ggml/llava/src/main.rs
@@ -0,0 +1,126 @@
+use serde_json::Value;
+use std::collections::HashMap;
+use std::env;
+use std::io;
+use wasi_nn::{self, GraphExecutionContext};
+
+fn read_input() -> String {
+    loop {
+        let mut answer = String::new();
+        io::stdin()
+            .read_line(&mut answer)
+            .expect("Failed to read line");
+        if !answer.is_empty() && answer != "\n" && answer != "\r\n" {
+            return answer.trim().to_string();
+        }
+    }
+}
+
+fn set_data_to_context(
+    context: &mut GraphExecutionContext,
+    data: Vec<u8>,
+) -> Result<(), wasi_nn::Error> {
+    context.set_input(0, wasi_nn::TensorType::U8, &[1], &data)
+}
+
+fn get_data_from_context(context: &GraphExecutionContext, index: usize) -> String {
+    // Preserve for 4096 tokens with average token length 6
+    const MAX_OUTPUT_BUFFER_SIZE: usize = 4096 * 6;
+    let mut output_buffer = vec![0u8; MAX_OUTPUT_BUFFER_SIZE];
+    let mut output_size = context
+        .get_output(index, &mut output_buffer)
+        .expect("Failed to get output");
+    output_size = std::cmp::min(MAX_OUTPUT_BUFFER_SIZE, output_size);
+
+    String::from_utf8_lossy(&output_buffer[..output_size]).to_string()
+}
+
+fn get_output_from_context(context: &GraphExecutionContext) -> String {
+    get_data_from_context(context, 0)
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let model_name: &str = &args[1];
+
+    // Set options for the graph. Check our README for more details:
+    // https://github.com/second-state/WasmEdge-WASINN-examples/tree/master/wasmedge-ggml#parameters
+    let mut options = HashMap::new();
+    options.insert("enable-log", Value::from(false));
+    options.insert("n-gpu-layers", Value::from(0));
+    options.insert("ctx-size", Value::from(2048));
+    if let Ok(val) = env::var("mmproj") {
+        options.insert("mmproj", Value::from(val.as_str()));
+    } else {
+        eprintln!("Failed to get mmproj model.");
+        std::process::exit(1);
+    }
+    if let Ok(val) = env::var("image") {
+        options.insert("image", Value::from(val.as_str()));
+    } else {
+        eprintln!("Failed to get the target image.");
+        std::process::exit(1);
+    }
+
+    // Create graph and initialize context.
+    let graph =
+        wasi_nn::GraphBuilder::new(wasi_nn::GraphEncoding::Ggml, wasi_nn::ExecutionTarget::AUTO)
+            .config(serde_json::to_string(&options).expect("Failed to serialize options"))
+            .build_from_cache(model_name)
+            .expect("Failed to build graph");
+    let mut context = graph
+        .init_execution_context()
+        .expect("Failed to init context");
+
+    let mut saved_prompt = String::new();
+    let system_prompt = String::from("You are a helpful, respectful and honest assistant. Always answer as short as possible, while being safe." );
+    let image_placeholder = "{WASMEDGE_IMAGE}";
+
+    loop {
+        println!("Question:");
+        let input = read_input();
+
+        // llava chat format is "<system_prompt>\nUSER:<image_embeddings>\n<textual_prompt>\nASSISTANT:"
+        if saved_prompt.is_empty() {
+            saved_prompt = format!(
+                "{}\nUSER:{}\n{}\nASSISTANT:",
+                system_prompt, image_placeholder, input
+            );
+        } else {
+            saved_prompt = format!("{}\nUSER: {}\nASSISTANT:", saved_prompt, input);
+        }
+
+        // Set prompt to the input tensor.
+        set_data_to_context(&mut context, saved_prompt.as_bytes().to_vec())
+            .expect("Failed to set input");
+
+        // Execute the inference.
+        let mut reset_prompt = false;
+        match context.compute() {
+            Ok(_) => (),
+            Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::ContextFull)) => {
+                println!("\n[INFO] Context full, we'll reset the context and continue.");
+                reset_prompt = true;
+            }
+            Err(wasi_nn::Error::BackendError(wasi_nn::BackendError::PromptTooLong)) => {
+                println!("\n[INFO] Prompt too long, we'll reset the context and continue.");
+                reset_prompt = true;
+            }
+            Err(err) => {
+                println!("\n[ERROR] {}", err);
+            }
+        }
+
+        // Retrieve the output.
+        let mut output = get_output_from_context(&context);
+        println!("Answer:\n{}", output.trim());
+
+        // Update the saved prompt.
+        if reset_prompt {
+            saved_prompt.clear();
+        } else {
+            output = output.trim().to_string();
+            saved_prompt = format!("{} {}", saved_prompt, output);
+        }
+    }
+}
