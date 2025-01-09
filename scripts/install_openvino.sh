a9a2b7583c239b2f05d513f4dbc213f08e5835f8
hydai <z54981220@gmail.com>
2025-01-10 00:57:56 +0800

    ci: install openvino 2024 instead of 2023
    Signed-off-by: hydai <z54981220@gmail.com>

--- scripts/install_openvino.sh
+++ scripts/install_openvino.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -e
-echo "Installing OpenVINO with version 2023.0.0"
-wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-echo "deb https://apt.repos.intel.com/openvino/2023 ubuntu20 main" | tee /etc/apt/sources.list.d/intel-openvino-2023.list
+echo "Installing OpenVINO with version 2024.2.0"
+KEY_FILE=GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+wget https://apt.repos.intel.com/intel-gpg-keys/$KEY_FILE && \
+    apt-key add $KEY_FILE && \
+    rm -f $KEY_FILE
+echo "deb https://apt.repos.intel.com/openvino/2024 ubuntu24 main" | tee /etc/apt/sources.list.d/intel-openvino-2024.list
 apt update
 apt upgrade -y
-apt search openvino
-apt-get -y install openvino-2023.0.2
+apt-get -y install openvino-2024.2.0
 export PATH=/usr/lib/x86_64-linux-gnu:$PATH
+ldconfig
